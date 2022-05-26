58f413f728ca8185703788abfdfaa957865d319a
iman <i.rezayan@gmail.com>
2022-05-26 22:46:52 +0430

    add logging

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template, abort, jsonify
-from werkzeug.utils import secure_filename
+import io
+import logging
+from flask import Flask, request, send_from_directory, render_template, abort, jsonify, Response
 from flask_restful import Api, Resource
 
-from flasgger import Swagger
-from flasgger.utils import swag_from
 from flask_restful_swagger import swagger
 
 import key
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -27,6 +32,23 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def log_creator(name, file_name, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    fh = logging.FileHandler(f"{file_name}.log")
+
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    fh.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    return logger
+
+
+code_logger = log_creator("code_info", "code_log")
+
+
 @app.route('/flash')
 def index():
     return render_template('index.html')
@@ -37,26 +59,29 @@ class FileTasks(Resource):
     @swagger.model
     @swagger.operation(notes="Upload a file")
     def post(self, name):
-
+        code_logger.info(f"Upload '{name}' file")
         if "/" in name:
-            # Return 400 BAD REQUEST
             abort(400, "no subdirectories allowed")
-
-        with open(os.path.join(UPLOAD_FOLDER, name), "wb") as fp:
-            fp.write(request.data)
-
-        # Return 201 CREATED
-        return {}, 201
+        if name != '':
+            with open(os.path.join(UPLOAD_FOLDER, name), "wb") as fp:
+                fp.write(request.data)
+            response = {}, 201
+        else:
+            code_logger.warning("Error in upload!")
+            response = {}, 400
+        return response
 
     @swagger.model
     @swagger.operation(notes="download a file")
     def get(self, name):
+        code_logger.info(f"Download '{name}' file")
         path = os.path.join(app.config['UPLOAD_FOLDER'], name)
         if os.path.exists(path):
             try:
                 return send_from_directory(app.config["UPLOAD_FOLDER"], name)
-            except Exception as e:
-                print(e)
+            except Exception as ex:
+                print(ex)
+                code_logger.exception("Error in download!")
                 return {}, 500
         else:
             return {}, 404
@@ -64,12 +89,18 @@ class FileTasks(Resource):
     @swagger.model
     @swagger.operation(notes="Delete a file")
     def delete(self, name):
-        path = os.path.join(app.config['UPLOAD_FOLDER'], name)
-        if os.path.exists(path):
-            os.remove(path)
-            response = {}, 200
-        else:
-            response = {}, 404
+        code_logger.info(f"Delete '{name}' file")
+        try:
+            path = os.path.join(app.config['UPLOAD_FOLDER'], name)
+            if os.path.exists(path):
+                os.remove(path)
+                response = {}, 200
+            else:
+                response = {}, 404
+        except Exception as ex:
+            print(ex)
+            code_logger.exception("Error in delete!")
+            response = {}, 500
         return response
 
 
@@ -82,17 +113,41 @@ class ListFiles(Resource):
         counter = 0
         for (_, _, filenames) in os.walk('./data'):
             list_of_files.extend(filenames)
-        # response_list = '\n'.join(list_of_files)
-        # return jsonify(render_template('list.html', messages=list_of_files))
         for item in list_of_files:
             counter += 1
             dic_of_files.update({counter: item})
 
+        code_logger.info("List Files")
         return jsonify(dic_of_files)
 
 
+class Diagram(Resource):
+
+    @swagger.model
+    @swagger.operation(notes="List Files")
+    def get(self, name, xo, yc):
+        kline = pd.read_csv(f"data/{name}")
+        try:
+            x = kline[xo][-10:].tolist()
+            y = kline[yc][-10:].tolist()
+        except KeyError:
+            return {}, 404
+        x = [x[5:12] for x in x]
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.plot(x, y)
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        return Response(output.getvalue(), mimetype='image/png')
+
+
+# There must be a way to handle empty file names
+
 api.add_resource(FileTasks, '/file/<string:name>', methods=['GET', 'POST', 'DELETE'])
+
 api.add_resource(ListFiles, '/list')
+
+api.add_resource(Diagram, '/draw/<string:name>/<string:xo>/<string:yc>')
 
 if __name__ == '__main__':
     app.run(debug=True)
